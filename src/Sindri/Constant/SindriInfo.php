7c40d1725ac7878771aba2bc94af711627e30b5f
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-06-18 05:28:37 +0000

    [Release] Update SindriInfo for v26.4.1 release

--- src/Sindri/Constant/SindriInfo.php
+++ src/Sindri/Constant/SindriInfo.php
@@ -20,14 +20,14 @@ final class SindriInfo
      *
      * @var non-empty-string
      */
-    public const string VERSION = '26.4.0';
+    public const string VERSION = '26.4.1';
 
     /**
      * The Sindri package version build datetime.
      *
      * @var non-empty-string
      */
-    public const string VERSION_BUILD_DATE_TIME = 'June 17 2026 22:15:43 MST';
+    public const string VERSION_BUILD_DATE_TIME = 'June 17 2026 22:28:00 MST';
 
     /**
      * The CLI banner icon (Mjölnir).
